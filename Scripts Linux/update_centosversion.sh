e5a5bc6cc19396b4829fe1bdb40ced4f0206d9bd
Kabelouze <145665599+Kabelouze@users.noreply.github.com>
2023-12-27 16:15:16 +0100

    Create update_centosversion.sh

---
+++ Scripts Linux/update_centosversion.sh
@@ -0,0 +1,15 @@
+#!/bin/bash #Interpreteur du shell
+
+yum makecache # Commande de mise à jour
+
+if [ $? != 1 ]; then # Si la commande passe = 1 sinon = 0 
+	# Ecrit dans le fichier de log la date de la mise à jour de la liste des paquets
+	echo "Update : Mise à jour de la liste des paquets faite le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.txt
+	yum upgrade -y # Commande de la mise à des paquets
+	# Ecrit dans le fichier de log la date de la mise à jour des paquets 
+	echo "Upgrade : Mise à jour des paquets faite le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.txt
+else
+	# Si les commande ne passe pas écrit dans le fichier de log la date de l'erreur
+	echo "Erreur de la mise à jour le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.txt
+
+fi # Fin du Script
