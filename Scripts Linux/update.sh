cb8a4f0a7ec2487b79b8160d0d2d1c23acf847d3
Kabelouze <145665599+Kabelouze@users.noreply.github.com>
2024-01-06 00:44:06 +0100

    Update update.sh

--- Scripts Linux/update.sh
+++ Scripts Linux/update.sh
@@ -4,12 +4,12 @@ sudo apt update # Commande de mise à jour
 
 if [ $? != 1 ]; then # Si la commande passe = 1 sinon = 0 
 	# Ecrit dans le fichier de log la date de la mise à jour de la liste des paquets
-	echo "Update : Mise à jour de la liste des paquets faite le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.txt
+	echo "Update $(hostname) : Mise à jour de la liste des paquets faite le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.log
 	sudo apt-get upgrade -y # Commande de la mise à des paquets
 	# Ecrit dans le fichier de log la date de la mise à jour des paquets 
-	echo "Upgrade : Mise à jour des paquets faite le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.txt
+	echo "Upgrade $(hostname) : Mise à jour des paquets faite le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.log
 else
 	# Si les commande ne passe pas écrit dans le fichier de log la date de l'erreur
-	echo "Erreur de la mise à jour le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.txt
+	echo "Erreur sur le serveur $(hostname) de la mise à jour le $(date +"%d/%m/%y %k:%M")" >> /var/log/logsscripts.log
 
 fi # Fin du Script
